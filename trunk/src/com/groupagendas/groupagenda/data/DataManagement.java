5622505b4268edcfc662dd87b2c554e23f9e756d
meska.lt@gmail.com <meska.lt@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-07-10 10:08:43 +0000

    All the states included.

--- trunk/src/com/groupagendas/groupagenda/data/DataManagement.java
+++ trunk/src/com/groupagendas/groupagenda/data/DataManagement.java
@@ -47,6 +47,46 @@ public class DataManagement {
 
 	public void fillStates() {
 		states.put("mobile/login", 1);
+		states.put("mobile/account_get", 2);
+		states.put("mobile/account_edit", 3);
+		states.put("mobile/account_email_change", 4);
+		states.put("mobile/account_password_change", 5);
+		states.put("mobile/account_image", 6);
+		states.put("mobile/account_register", 7);
+		states.put("mobile/groups_list", 8);
+		states.put("mobile/groups_create", 9);
+		states.put("mobile/groups_edit", 10);
+		states.put("mobile/group_get", 11);
+		states.put("mobile/group_remove", 12);
+		states.put("mobile/contact_list", 13);
+		states.put("mobile/contact_get", 14);
+		states.put("mobile/contact_create", 15);
+		states.put("mobile/contact_edit", 16);
+		states.put("mobile/contact_copy", 17);
+		states.put("mobile/contact_remove", 18);
+		states.put("mobile/events_list", 19);
+		states.put("mobile/events_get", 20);
+		states.put("mobile/events_create", 21);
+		states.put("mobile/events_edit", 22);
+		states.put("mobile/events_remove", 23);
+		states.put("mobile/set_event_status", 24);
+		states.put("mobile/events_invite_extra", 25);
+		states.put("mobile/settings_update", 26);
+		states.put("mobile/settings_set_autocolors", 27);
+		states.put("mobile/settings_set_autoicons", 28);
+		states.put("mobile/events_list_by_contact_id", 29);
+		states.put("mobile/events_change_time", 30);
+		states.put("mobile/events_purge_native", 31);
+		states.put("mobile/report_events", 32);
+		states.put("mobile/chat_get", 33);
+		states.put("mobile/chat_post", 34);
+		states.put("mobile/chat_remove", 35);
+		states.put("mobile/chat_threads", 36);
+		states.put("mobile/register_android", 37);
+		states.put("mobile/account_forgot_pass1", 38);
+		states.put("mobile/account_forgot_pass2", 39);
+		states.put("mobile/get_country_code", 40);
+		states.put("mobile/set_lastlogin", 41);
 	}
 
 	/**
@@ -113,7 +153,7 @@ public class DataManagement {
 	}
 
 	private void loginTask() {
-
+		
 	}
 
 }
