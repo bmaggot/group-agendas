98db7dfa6d95c116e15b7f4dad72f9e4922d61d6
Interfectos@gmail.com <Interfectos@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-07-10 11:27:54 +0000

    log in.

--- trunk/src/com/groupagendas/groupagenda/data/DataManagement.java
+++ trunk/src/com/groupagendas/groupagenda/data/DataManagement.java
@@ -2,7 +2,6 @@ package com.groupagendas.groupagenda.data;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.apache.http.HttpResponse;
@@ -14,13 +13,20 @@ import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
-
-import com.groupagendas.groupagenda.account.Account;
-import com.groupagendas.groupagenda.account.AccountProvider;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.google.android.c2dm.C2DMessaging;
+import com.groupagendas.groupagenda.account.Account;
+import com.groupagendas.groupagenda.account.AccountProvider;
 
 public class DataManagement {
 
@@ -108,19 +114,27 @@ public class DataManagement {
 	 * @return request state (successful or not)
 	 * @see MultipartEntity
 	 */
-	public boolean connect(String path, ArrayList<String[]> parts) {
+	public boolean connect(String path, HashMap<String, String> parts) {
 
 		boolean success = false;
 		HttpClient hc = new DefaultHttpClient();
 		HttpPost post = new HttpPost(Data.getDEFAULT_SERVER_URL() + path);
 
+		post.setHeader("User-Agent", "Android");
+		post.setHeader("Accept", "text/html,application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5");
 		MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
 
-		for (String[] part : parts) {
-			try {
-				reqEntity.addPart(part[1], new StringBody(part[2]));
-			} catch (UnsupportedEncodingException e) {
-				e.printStackTrace();
+		if (parts != null && !parts.isEmpty()) {
+			if (parts.containsKey("email") && parts.containsKey("password")) {
+				Data.setEmail(parts.get("email"));
+				Data.setPassword(parts.get("password"));
+			}
+			for (int i = 0; i < parts.size() - 1; i++) {
+				try {
+					reqEntity.addPart(parts.keySet().toArray()[i].toString(), new StringBody(parts.values().toArray()[2].toString()));
+				} catch (UnsupportedEncodingException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 
@@ -137,8 +151,8 @@ public class DataManagement {
 
 		if (rp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 			success = true;
-			try{
-				executeTask(path);
+			try {
+				executeTask(path, rp);
 			} catch (Exception e) {
 				e.getMessage();
 			}
@@ -147,26 +161,118 @@ public class DataManagement {
 		}
 		return success;
 	}
-	
-	private void executeTask (String path) throws Exception{
+
+	private void executeTask(String path, HttpResponse rp) throws Exception {
 		switch (states.get(path)) {
 		case 1:
-//			loginTask();
+			loginTask(rp);
 			break;
 		case 2:
 			getAccountTask();
 			break;
+		case 37:
+			break;
+
+		}
+	}
+
+	private void loginTask(HttpResponse rp) {
+		boolean success = false;
+		String token = null;
+		try {
+			String resp = EntityUtils.toString(rp.getEntity());
+			if (resp != null) {
+				JSONObject object = new JSONObject(resp);
+				success = object.getBoolean("success");
+				Log.e("resp", resp);
+				if (success == true) {
+					token = object.getString("token");
+					JSONObject profile = object.getJSONObject("profile");
+					int id = Integer.parseInt(profile.getString("user_id"));
+					Data.setToken(token);
+					Data.setUserId(id);
+					//
+					Data.setLogged(true);
+					//
+					Data.save();
+
+					// autoicons and autocolors
+					Data.getmContext().getContentResolver().delete(AccountProvider.AMetaData.AutoiconMetaData.CONTENT_URI, "", null);
+					JSONArray autoicons = object.getJSONArray("custom_icons");
+					for (int i = 0, l = autoicons.length(); i < l; i++) {
+						final JSONObject autoicon = autoicons.getJSONObject(i);
+						ContentValues values = new ContentValues();
+						values.put(AccountProvider.AMetaData.AutoiconMetaData.ICON, autoicon.getString("icon"));
+						values.put(AccountProvider.AMetaData.AutoiconMetaData.KEYWORD, autoicon.getString("keyword"));
+						values.put(AccountProvider.AMetaData.AutoiconMetaData.CONTEXT, autoicon.getString("context"));
+
+						Data.getmContext().getContentResolver().insert(AccountProvider.AMetaData.AutoiconMetaData.CONTENT_URI, values);
+					}
+
+					Data.getmContext().getContentResolver().delete(AccountProvider.AMetaData.AutocolorMetaData.CONTENT_URI, "", null);
+					JSONArray autocolors = object.getJSONArray("custom_colors");
+					for (int i = 0, l = autocolors.length(); i < l; i++) {
+						final JSONObject autocolor = autocolors.getJSONObject(i);
+						ContentValues values = new ContentValues();
+						values.put(AccountProvider.AMetaData.AutocolorMetaData.COLOR, autocolor.getString("color"));
+						values.put(AccountProvider.AMetaData.AutocolorMetaData.KEYWORD, autocolor.getString("keyword"));
+						values.put(AccountProvider.AMetaData.AutocolorMetaData.CONTEXT, autocolor.getString("context"));
+
+						Data.getmContext().getContentResolver().insert(AccountProvider.AMetaData.AutocolorMetaData.CONTENT_URI, values);
+					}
+					registerPhone();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void registerPhone() {
+		try {
+			getImei(Data.getmContext());
+			Data.setPushId(C2DMessaging.getRegistrationId(Data.getmContext()));
+			if (Data.getPushId() == "") {
+				System.out.println("C2DMessaging.register()");
+
+				C2DMessaging.register(Data.getmContext(), "group.agenda.c2dm@gmail.com");
+			} else {
+				sendPushIdToServer(Data.getmContext(), Data.getPushId());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static String getImei(Context context) {
+
+		try {
+			TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+			if (telephonyManager == null) {
+				return "";
+			}
+
+			return telephonyManager.getDeviceId();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		return null;
+	}
+
+	private static void sendPushIdToServer(Context context, String pushId) {
+
+		try {
+			DataManagement._instance.connect("mobile/register_android", null);
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 	}
 
 	private void getAccountTask() {
 		Account u = null;
-		Cursor result = Data.getmContext().getContentResolver().query(
-				AccountProvider.AMetaData.AccountMetaData.CONTENT_URI,
-				null,
-				null,
-				null,
-				null);
+		Cursor result = Data.getmContext().getContentResolver()
+				.query(AccountProvider.AMetaData.AccountMetaData.CONTENT_URI, null, null, null, null);
 
 		if (result.moveToFirst()) {
 			u = new Account();
