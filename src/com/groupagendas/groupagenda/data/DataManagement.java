5da8f2e16efd82f67ee7482a1f1c63015a3d2978
Interfectos <Interfectos@gmail.com>
2012-07-10 08:56:32 +0000

    einam parookyt ((;

--- src/com/groupagendas/groupagenda/data/DataManagement.java
+++ src/com/groupagendas/groupagenda/data/DataManagement.java
@@ -3,6 +3,7 @@ package com.groupagendas.groupagenda.data;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
@@ -14,34 +15,64 @@ import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 
+import android.app.Activity;
+import android.content.Context;
+
 public class DataManagement {
 
-	public DataManagement (){
-		
+	private static DataManagement _instance = null;
+	private static final HashMap<String, Integer> states = new HashMap<String, Integer>();
+
+	private DataManagement(Activity c) {
+		Data.set_prefs(c.getSharedPreferences("PREFS_PRIVATE", Context.MODE_PRIVATE));
+		Data.setmContext(c);
 	}
-	
+
+	private DataManagement(Context c) {
+		Data.set_prefs(c.getSharedPreferences("PREFS_PRIVATE", Context.MODE_PRIVATE));
+		Data.setmContext(c);
+	}
+
+	public static synchronized DataManagement getInstance(Activity c) {
+		if (_instance == null)
+			_instance = new DataManagement(c);
+		return _instance;
+	}
+
+	public static synchronized DataManagement getInstance(Context c) {
+		if (_instance == null)
+			_instance = new DataManagement(c);
+		return _instance;
+	}
+
+	public void fillStates() {
+		states.put("mobile/login", 1);
+	}
+
 	/**
-	 * Returns an boolean that determines if connection to server was successful. 
-	 * The path argument must specify a relative link to project's API. The parts
-	 * argument contains request data. 
+	 * Returns an boolean that determines if connection to server was
+	 * successful. The path argument must specify a relative link to project's
+	 * API. The parts argument contains request data.
 	 * 
-	 * This method always returns immediately, whether or not the 
-	 * connection was successful.
+	 * This method always returns immediately, whether or not the connection was
+	 * successful.
 	 * 
-	 * @param  path  an absolute URL giving the base location of the image
-	 * @param  parts the location of the image, relative to the url argument
-	 * @return      request state (successful or not)
-	 * @see         MultipartEntity
+	 * @param path
+	 *            an absolute URL giving the base location of the image
+	 * @param parts
+	 *            the location of the image, relative to the url argument
+	 * @return request state (successful or not)
+	 * @see MultipartEntity
 	 */
-	public boolean connect(String path, ArrayList<String[]> parts){
-		
+	public boolean connect(String path, ArrayList<String[]> parts) {
+
 		boolean success = false;
 		HttpClient hc = new DefaultHttpClient();
 		HttpPost post = new HttpPost(Data.getDEFAULT_SERVER_URL() + path);
 
 		MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
 
-		for(String[] part : parts){
+		for (String[] part : parts) {
 			try {
 				reqEntity.addPart(part[1], new StringBody(part[2]));
 			} catch (UnsupportedEncodingException e) {
@@ -60,11 +91,29 @@ public class DataManagement {
 			e.printStackTrace();
 		}
 
-		if(rp.getStatusLine().getStatusCode() == HttpStatus.SC_OK){
+		if (rp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
 			success = true;
+			try{
+				executeTask(path);
+			} catch (Exception e) {
+				e.getMessage();
+			}
 		} else {
 			success = false;
 		}
 		return success;
 	}
+	
+	private void executeTask (String path) throws Exception{
+		switch (states.get(path)) {
+		case 1:
+			loginTask();
+			break;
+		}
+	}
+
+	private void loginTask() {
+
+	}
+
 }
