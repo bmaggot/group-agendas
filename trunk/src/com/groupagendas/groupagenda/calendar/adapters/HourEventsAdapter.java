4e3d8658e1d0e0cd36c075b8a5c852ae551a4b0a
Justinas.Marcinka@gmail.com <Justinas.Marcinka@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-07-18 10:59:43 +0000

    projektas pagal Justa

--- trunk/src/com/groupagendas/groupagenda/calendar/adapters/HourEventsAdapter.java
+++ trunk/src/com/groupagendas/groupagenda/calendar/adapters/HourEventsAdapter.java
@@ -2,6 +2,7 @@ package com.groupagendas.groupagenda.calendar.adapters;
 
 import java.util.List;
 
+import com.groupagendas.groupagenda.R;
 import com.groupagendas.groupagenda.events.Event;
 
 import android.content.Context;
@@ -12,13 +13,16 @@ public class HourEventsAdapter extends AbstractAdapter {
 
 	public HourEventsAdapter(Context context, List<Event> list) {
 		super(context, list);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public View getView(int i, View view, ViewGroup viewGroup) {
-		// TODO Auto-generated method stub
-		return null;
+		if (view == null) {
+//           TODO view = mInflater.inflate(R.layout.calendar_day_entry, null);
+        }
+		final Event event = list.get(i);
+		
+		return view;
 	}
 
 }
