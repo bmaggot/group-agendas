fa4e823885c6efd78f2724a045345fdb1f38fdcc
Justinas.Marcinka <Justinas.Marcinka@gmail.com>
2012-07-18 06:50:33 +0000

    declared alldayEvents and hourEvents adapters in project and dayview class

---
+++ src/com/groupagendas/groupagenda/calendar/adapters/HourEventsAdapter.java
@@ -0,0 +1,24 @@
+package com.groupagendas.groupagenda.calendar.adapters;
+
+import java.util.List;
+
+import com.groupagendas.groupagenda.events.Event;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class HourEventsAdapter extends AbstractAdapter {
+
+	public HourEventsAdapter(Context context, List<Event> list) {
+		super(context, list);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public View getView(int i, View view, ViewGroup viewGroup) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
