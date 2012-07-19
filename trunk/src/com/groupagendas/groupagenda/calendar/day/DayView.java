0cc18b3012c4a8aef23e2693c2a98d9153777a96
Justinas.Marcinka@gmail.com <Justinas.Marcinka@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-07-19 13:57:29 +0000

    unnecesarry stuff deleted

--- trunk/src/com/groupagendas/groupagenda/calendar/day/DayView.java
+++ trunk/src/com/groupagendas/groupagenda/calendar/day/DayView.java
@@ -1,12 +1,9 @@
 package com.groupagendas.groupagenda.calendar.day;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
+
+
+
 import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
 
 import android.app.Activity;
 import android.content.Context;
@@ -17,20 +14,14 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.bog.calendar.app.model.EventListAdapter;
 import com.groupagendas.groupagenda.R;
 import com.groupagendas.groupagenda.calendar.adapters.AllDayEventsAdapter;
 import com.groupagendas.groupagenda.calendar.adapters.HourEventsAdapter;
-import com.groupagendas.groupagenda.data.Data;
-import com.groupagendas.groupagenda.data.DataManagement;
-import com.groupagendas.groupagenda.events.Event;
-import com.groupagendas.groupagenda.utils.Utils;
 
 public class DayView extends LinearLayout {
 	
 	DayInstance selectedDay;
 
-//	Calendar selectedDate = Calendar.getInstance();
 	ImageButton prevDayButton;
 	ImageButton nextDaybutton;
 	TextView topPanelTitle;
@@ -52,7 +43,6 @@ public class DayView extends LinearLayout {
 		WeekDayNames = getResources().getStringArray(R.array.week_days_names);
 		MonthNames = getResources().getStringArray(R.array.month_names);
 		selectedDay = new DayInstance(context);
-//		selectedDate.setFirstDayOfWeek(Data.DEFAULT_FIRST_WEEK_DAY);
 
 	}
 
@@ -60,99 +50,17 @@ public class DayView extends LinearLayout {
 	protected void onFinishInflate() {
 		super.onFinishInflate();
 		
-		((Activity) getContext()).getLayoutInflater().inflate(R.layout.calendar_day_rewrite, this);
+		((Activity) getContext()).getLayoutInflater().inflate(R.layout.calendar_day, this);
 		setupViewItems();
 		initEventListAdapters();
-//		initEventListAdapters(selectedDate);
-		// initEventListAdapter(selectedDate, false);
-		// initEventListAdapter(selectedDate, true);
+
 	}
 
 	private void initEventListAdapters() {
 		allDayEventAdapter.setList(selectedDay.getAllDayEvents());
 		allDayEventAdapter.notifyDataSetChanged();
-//		allDayEventAdapter.setList(selectedDay.getHourEvents()());
-//		allDayEventAdapter.notifyDataSetChanged();
-		
-	}
-
-	private void initEventListAdapters(Calendar selectedDate) {
-		ArrayList<Event> events = Data.getEvents();
-//		System.out.println("got data. size: " + events.size());
-		// hourEventAdapter.setList(filterHourEvents(events, selectedDate));
-		// dayEventsPanel.setAdapter(hourEventAdapter);
-		// TODO hourEventAdapter.notifyDataSetChanged();
-
-//		filterHourEvents(events, selectedDate);
-		List<Event> ev = filterAllDayEvents(events, selectedDate);
-//		System.out.println("show data " + ev.size());
-		allDayEventAdapter.setList(ev);
-		allDayEventAdapter.notifyDataSetChanged();
-		
 
 		
-
-	}
-
-//	private void testAdapter(ArrayList<Event> events) {
-////		System.out.println("EVENTU: " + events.size());
-////		allDayEventAdapter.setList(events);
-////		allDayEventAdapter.notifyDataSetChanged();
-//
-//	}
-
-	private List<Event> filterAllDayEvents(ArrayList<Event> events, Calendar date) {
-		ArrayList<Event> allDayEvents = new ArrayList<Event>();
-
-		Calendar currentDay = Calendar.getInstance();
-		String dayStr = new SimpleDateFormat("yyyy-MM-dd").format(date.getTime());
-		Calendar day_start = Utils.stringToCalendar(dayStr + " 00:00:00", Utils.date_format);
-		Calendar day_end = Utils.stringToCalendar(dayStr + " 23:59:59", Utils.date_format);
-		Calendar event_start = null;
-		Calendar event_end = null;
-
-		for (Event event : events) {
-
-			if (!event.my_time_end.equals("null") && !event.my_time_start.equals("null")) {
-				event_start = Utils.stringToCalendar(event.my_time_start, event.timezone, Utils.date_format);
-				event_end = Utils.stringToCalendar(event.my_time_end, event.timezone, Utils.date_format);
-
-				if (!event_end.before(day_start) && !event_start.after(day_end)) {
-					if ((event_end.getTime().after(day_end.getTime()) || event_end.getTime().equals(day_end.getTime()))
-							&& (event_start.getTime().before(day_start.getTime()) || event_start.getTime().equals(day_start.getTime()))
-							|| event.is_all_day) {
-						allDayEvents.add(event);
-//						System.out.println("added all day event        " + event.title);
-					}
-				}
-			}
-		}
-		return allDayEvents;
-	}
-
-	private List<Event> filterHourEvents(ArrayList<Event> events, Calendar date) {
-		ArrayList<Event> dayEvents = new ArrayList<Event>();
-		String dayStr = new SimpleDateFormat("yyyy-MM-dd").format(date.getTime());
-		Calendar day_start = Utils.stringToCalendar(dayStr + " 00:00:00", Utils.date_format);
-		Calendar day_end = Utils.stringToCalendar(dayStr + " 23:59:59", Utils.date_format);
-		Calendar event_start = null;
-		Calendar event_end = null;
-		for (Event event : events) {
-			if (!event.my_time_end.equals("null") && !event.my_time_start.equals("null")) {
-				event_start = Utils.stringToCalendar(event.my_time_start, event.timezone, Utils.date_format);
-				event_end = Utils.stringToCalendar(event.my_time_end, event.timezone, Utils.date_format);
-				if(day_start.getTime().before(event_start.getTime())){
-					
-				}
-				if ((day_start.getTime().before(event_start.getTime()) || day_start.getTime().equals(event_start.getTime()))
-						&& (day_end.getTime().after(event_end.getTime()) || day_end.getTime().equals(event_end.getTime())) 
-						&& !event.is_all_day) {
-					dayEvents.add(event);
-//					System.out.println("added hour event        " + event.title);
-				}
-			}
-		}
-		return dayEvents;
 	}
 
 	private void setupViewItems() {
@@ -172,11 +80,9 @@ public class DayView extends LinearLayout {
 
 			@Override
 			public void onClick(View v) {
-//				selectedDate.add(Calendar.DATE, -1);
 				selectedDay.goPrev();
 				updateTopPanelTitle(selectedDay.getSelectedDate());
 				initEventListAdapters();
-//				initEventListAdapters(selectedDate);
 			}
 		});
 
@@ -184,11 +90,9 @@ public class DayView extends LinearLayout {
 
 			@Override
 			public void onClick(View v) {
-//				selectedDate.add(Calendar.DATE, 1);
 				selectedDay.goNext();
 				updateTopPanelTitle(selectedDay.getSelectedDate());
 				initEventListAdapters();
-//				initEventListAdapters(selectedDate);
 
 			}
 		});
