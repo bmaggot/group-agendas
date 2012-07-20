4cb36154d3703d5a1069ecbb029d032f98de0e55
Justinas.Marcinka@gmail.com <Justinas.Marcinka@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-07-20 07:07:47 +0000

    DayView class: added drawHourEventsMethod() Not yet implemented
    Added custom HourEventView NOT YET IMPLEMENTED

---
+++ trunk/src/com/groupagendas/groupagenda/calendar/day/HourEventView.java
@@ -0,0 +1,18 @@
+package com.groupagendas.groupagenda.calendar.day;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class HourEventView extends View {
+	public HourEventView(Context context){
+		super (context);
+		System.out.println("test");
+	}
+
+	public HourEventView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
