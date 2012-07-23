7290772536ae905416d4084af87b1c46fbe3f6a1
Justinas.Marcinka@gmail.com <Justinas.Marcinka@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-07-23 08:46:12 +0000

    created class for hour events panel

---
+++ trunk/src/com/groupagendas/groupagenda/calendar/day/HourEventsPanel.java
@@ -0,0 +1,13 @@
+package com.groupagendas.groupagenda.calendar.day;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+public class HourEventsPanel extends RelativeLayout {
+
+	public HourEventsPanel(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+}
