e7b3c60cf883a42befc2d3ad525c90e5c0fb39bf
Interfectos <Interfectos@gmail.com>
2012-07-23 09:23:49 +0000

    background working

--- src/com/groupagendas/groupagenda/NavbarActivity.java
+++ src/com/groupagendas/groupagenda/NavbarActivity.java
@@ -14,6 +14,7 @@ import android.os.Handler;
 import android.os.Message;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -540,5 +541,14 @@ public class NavbarActivity extends Activity {
         public void setState(int state) {
             mState = state;
         }
+    }
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            moveTaskToBack(true);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
