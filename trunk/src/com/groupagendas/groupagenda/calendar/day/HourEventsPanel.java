80ea7f01a2ac6657ca4907da57fa7a7ec6b25323
Justinas.Marcinka@gmail.com <Justinas.Marcinka@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-07-23 09:08:03 +0000

    constructor modification

--- trunk/src/com/groupagendas/groupagenda/calendar/day/HourEventsPanel.java
+++ trunk/src/com/groupagendas/groupagenda/calendar/day/HourEventsPanel.java
@@ -1,13 +1,19 @@
 package com.groupagendas.groupagenda.calendar.day;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.widget.RelativeLayout;
 
 public class HourEventsPanel extends RelativeLayout {
 
-	public HourEventsPanel(Context context) {
-		super(context);
-		// TODO Auto-generated constructor stub
+	public HourEventsPanel(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		System.out.println("KONSTRUKTORIUS ISKVIECIAMAS");
 	}
 
+	public HourEventsPanel(Context context) {
+		this(context, null);
+	}
+	
+	
 }
