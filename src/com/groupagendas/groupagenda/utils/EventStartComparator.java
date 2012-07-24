253d786e581de2d14e548c1e7d9722d4515c7755
Justinas.Marcinka <Justinas.Marcinka@gmail.com>
2012-07-24 14:15:04 +0000

    added comparator that compares two event by start dates

---
+++ src/com/groupagendas/groupagenda/utils/EventStartComparator.java
@@ -0,0 +1,21 @@
+package com.groupagendas.groupagenda.utils;
+
+import java.util.Calendar;
+import java.util.Comparator;
+
+import com.groupagendas.groupagenda.events.Event;
+
+public class EventStartComparator implements Comparator<Event> {
+
+	@Override
+	public int compare(Event event1, Event event2) {
+		Calendar start1 = Utils.stringToCalendar(event1.time_start, Utils.date_format);
+		Calendar start2 = Utils.stringToCalendar(event2.time_start, Utils.date_format);
+		
+		
+		return (start1.before(start2) ? -1 : (start2.before(start1) ? 1 : 0));
+	}
+
+	
+
+}
