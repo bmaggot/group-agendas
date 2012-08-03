bab29f79f0b96e9f26d98f2705399733934b8e69
meska.lt <meska.lt@gmail.com>
2012-08-03 13:33:19 +0000

    Reverting Google API includes.

--- src/com/groupagendas/groupagenda/contacts/ContactsActivity.java
+++ src/com/groupagendas/groupagenda/contacts/ContactsActivity.java
@@ -29,21 +29,10 @@ import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 import android.widget.TextView;
 
-import com.google.gdata.client.contacts.ContactsService;
-import com.google.gdata.util.AuthenticationException;
-import com.google.gdata.util.ServiceException;
 import com.groupagendas.groupagenda.R;
 import com.groupagendas.groupagenda.data.DataManagement;
 import com.makeramen.segmented.SegmentedRadioGroup;
 
-import com.google.gdata.client.*;
-import com.google.gdata.client.contacts.*;
-import com.google.gdata.data.*;
-import com.google.gdata.data.contacts.*;
-import com.google.gdata.data.contacts.ContactEntry;
-import com.google.gdata.data.contacts.ContactFeed;
-import com.google.gdata.data.extensions.*;
-import com.google.gdata.util.*;
 import java.io.IOException;
 import java.net.URL;
 
@@ -187,153 +176,12 @@ public class ContactsActivity extends ListActivity implements OnCheckedChangeLis
 			
 			@Override
 			public void onClick(View v) {
-				ContactsService myService = new ContactsService("<var>GA</var>");
-				try {
-					myService.setUserCredentials("group.agenda.c2dm@gmail.com", "abcdeqwertabc123");
-					try {
-						printAllContacts(myService);
-					} catch (ServiceException e) {
-						e.printStackTrace();
-						System.out.println("ServiceException");
-						e.getMessage();
-						e.getCause();
-						e.getInternalReason();
-						System.out.println("ServiceException");
-					} catch (IOException e) {
-						e.printStackTrace();
-						System.out.println("IOException");
-						e.getMessage();
-						e.getCause();
-						System.out.println("IOException");
-					}
-				} catch (AuthenticationException e) {
-					e.printStackTrace();
-					System.out.println("AuthenticationException");
-					e.getMessage();
-					e.getInternalReason();
-					System.out.println("AuthenticationException");
-				}
-				
+//				printAllContacts(myService);
 			}
 		});
 		
 	}
 	
-	public static void printAllContacts(ContactsService myService)
-		    throws ServiceException, IOException {
-		  // Request the feed
-		  URL feedUrl = new URL("https://www.google.com/m8/feeds/contacts/default/full");
-		  ContactFeed resultFeed = myService.getFeed(feedUrl, ContactFeed.class);
-		  // Print the results
-		  System.out.println(resultFeed.getTitle().getPlainText());
-		  for (ContactEntry entry : resultFeed.getEntries()) {
-		    if (entry.hasName()) {
-		      Name name = entry.getName();
-		      if (name.hasFullName()) {
-		        String fullNameToDisplay = name.getFullName().getValue();
-		        if (name.getFullName().hasYomi()) {
-		          fullNameToDisplay += " (" + name.getFullName().getYomi() + ")";
-		        }
-		      System.out.println("\\\t\\\t" + fullNameToDisplay);
-		      } else {
-		        System.out.println("\\\t\\\t (no full name found)");
-		      }
-		      if (name.hasNamePrefix()) {
-		        System.out.println("\\\t\\\t" + name.getNamePrefix().getValue());
-		      } else {
-		        System.out.println("\\\t\\\t (no name prefix found)");
-		      }
-		      if (name.hasGivenName()) {
-		        String givenNameToDisplay = name.getGivenName().getValue();
-		        if (name.getGivenName().hasYomi()) {
-		          givenNameToDisplay += " (" + name.getGivenName().getYomi() + ")";
-		        }
-		        System.out.println("\\\t\\\t" + givenNameToDisplay);
-		      } else {
-		        System.out.println("\\\t\\\t (no given name found)");
-		      }
-		      if (name.hasAdditionalName()) {
-		        String additionalNameToDisplay = name.getAdditionalName().getValue();
-		        if (name.getAdditionalName().hasYomi()) {
-		          additionalNameToDisplay += " (" + name.getAdditionalName().getYomi() + ")";
-		        }
-		        System.out.println("\\\t\\\t" + additionalNameToDisplay);
-		      } else {
-		        System.out.println("\\\t\\\t (no additional name found)");
-		      }
-		      if (name.hasFamilyName()) {
-		        String familyNameToDisplay = name.getFamilyName().getValue();
-		        if (name.getFamilyName().hasYomi()) {
-		          familyNameToDisplay += " (" + name.getFamilyName().getYomi() + ")";
-		        }
-		        System.out.println("\\\t\\\t" + familyNameToDisplay);
-		      } else {
-		        System.out.println("\\\t\\\t (no family name found)");
-		      }
-		      if (name.hasNameSuffix()) {
-		        System.out.println("\\\t\\\t" + name.getNameSuffix().getValue());
-		      } else {
-		        System.out.println("\\\t\\\t (no name suffix found)");
-		      }
-		    } else {
-		      System.out.println("\t (no name found)");
-		    }
-		    System.out.println("Email addresses:");
-		    for (Email email : entry.getEmailAddresses()) {
-		      System.out.print(" " + email.getAddress());
-		      if (email.getRel() != null) {
-		        System.out.print(" rel:" + email.getRel());
-		      }
-		      if (email.getLabel() != null) {
-		        System.out.print(" label:" + email.getLabel());
-		      }
-		      if (email.getPrimary()) {
-		        System.out.print(" (primary) ");
-		      }
-		      System.out.print("\n");
-		    }
-		    System.out.println("IM addresses:");
-		    for (Im im : entry.getImAddresses()) {
-		      System.out.print(" " + im.getAddress());
-		      if (im.getLabel() != null) {
-		        System.out.print(" label:" + im.getLabel());
-		      }
-		      if (im.getRel() != null) {
-		        System.out.print(" rel:" + im.getRel());
-		      }
-		      if (im.getProtocol() != null) {
-		        System.out.print(" protocol:" + im.getProtocol());
-		      }
-		      if (im.getPrimary()) {
-		        System.out.print(" (primary) ");
-		      }
-		      System.out.print("\n");
-		    }
-		    System.out.println("Groups:");
-		    for (GroupMembershipInfo group : entry.getGroupMembershipInfos()) {
-		      String groupHref = group.getHref();
-		      System.out.println("  Id: " + groupHref);
-		    }
-		    System.out.println("Extended Properties:");
-		    for (ExtendedProperty property : entry.getExtendedProperties()) {
-		      if (property.getValue() != null) {
-		        System.out.println("  " + property.getName() + "(value) = " +
-		            property.getValue());
-		      } else if (property.getXmlBlob() != null) {
-		        System.out.println("  " + property.getName() + "(xmlBlob)= " +
-		            property.getXmlBlob().getBlob());
-		      }
-		    }
-		    Link photoLink = entry.getContactPhotoLink();
-		    String photoLinkHref = photoLink.getHref();
-		    System.out.println("Photo Link: " + photoLinkHref);
-		    if (photoLink.getEtag() != null) {
-		      System.out.println("Contact Photo's ETag: " + photoLink.getEtag());
-		    }
-		    System.out.println("Contact's ETag: " + entry.getEtag());
-		  }
-		}
-
 	public void onPause() {
 		super.onPause();
 		editor.putString("ContactsActivityTask", CURRENT_TASK);
