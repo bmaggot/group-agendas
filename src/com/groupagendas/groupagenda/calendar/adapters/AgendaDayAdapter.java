a245336fe20ae5e342dda6b829d1df921d8eea97
Justinas.Marcinka <Justinas.Marcinka@gmail.com>
2012-08-20 13:07:34 +0000

    Bubbles are now good size

--- src/com/groupagendas/groupagenda/calendar/adapters/AgendaDayAdapter.java
+++ src/com/groupagendas/groupagenda/calendar/adapters/AgendaDayAdapter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,7 +15,7 @@ import com.groupagendas.groupagenda.R;
 import com.groupagendas.groupagenda.events.Event;
 
 public class AgendaDayAdapter extends AbstractAdapter<Event> {
-
+	
 	public AgendaDayAdapter(Context context, List<Event> list) {
 		super(context, list);
 	}
@@ -25,7 +26,6 @@ public class AgendaDayAdapter extends AbstractAdapter<Event> {
 			view = (LinearLayout)mInflater.inflate(R.layout.calendar_agenda_entry, null);
         }
 
-		
 		final Event event = list.get(i);
 		TextView text = (TextView) (view.findViewById(R.id.agenda_entry_title_placeholder));
 		text.setText(event.title);
