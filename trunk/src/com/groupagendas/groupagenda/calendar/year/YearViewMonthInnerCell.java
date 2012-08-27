563a96eb3dff6f7291dccbf8883f94cc226b9cad
Justinas.Marcinka@gmail.com <Justinas.Marcinka@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-08-27 08:50:39 +0000

    optimized year view

--- trunk/src/com/groupagendas/groupagenda/calendar/year/YearViewMonthInnerCell.java
+++ trunk/src/com/groupagendas/groupagenda/calendar/year/YearViewMonthInnerCell.java
@@ -6,11 +6,12 @@ import com.groupagendas.groupagenda.calendar.MonthCellState;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class YearViewMonthInnerCell extends RelativeLayout {
+public class YearViewMonthInnerCell extends RelativeLayout{
 	TextView text;
 	ImageView eventsIndicator;
 	boolean isWeekNum;
@@ -29,16 +30,10 @@ public class YearViewMonthInnerCell extends RelativeLayout {
 			int defStyle) {
 		super(context, attrs, defStyle);
 	}
-	/**
-	 * Used to setup weekNUm
-	 * @param isWeekNum
-	 * @param text
-	 */
-	public void setupWeekNum(String text){
+
+	public void setDayNum(String text){
 		initTextField();
-		this.isWeekNum = true;
 		this.text.setText(text);
-		refresh();
 	}
 	/**
 	 * Used to setup day celll
@@ -48,13 +43,11 @@ public class YearViewMonthInnerCell extends RelativeLayout {
 	public void setupDayCell(String text, MonthCellState state){
 		initTextField();
 		this.eventsIndicator = (ImageView) findViewById(R.id.indicator);
-		this.isWeekNum = false;
 		this.text.setText(text);
 		setState(state);
 	}
 	
 	public void setHasEvents(boolean bool){
-		if (isWeekNum) return;
 		hasEvents = bool;
 		if (hasEvents){
 			eventsIndicator.setVisibility(VISIBLE);
@@ -77,11 +70,6 @@ public class YearViewMonthInnerCell extends RelativeLayout {
 
 	private void refresh() {
 		this.setVisibility(VISIBLE);
-		if (isWeekNum){
-			text.setTextAppearance(getContext(), R.style.yearview_monthviewinnercell_w);
-			this.setBackgroundColor(R.color.lighter_gray);
-			return;
-		}
 		
 		switch (state){
 		case SELECTED:
