c206958cb091ef78fa007166bfc0e3f5b9fa5c28
meska.lt@gmail.com <meska.lt@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-08-28 14:38:06 +0000

    Networking on main thread has been moved into AsyncTasks.

--- trunk/src/com/groupagendas/groupagenda/contacts/importer/RequestTokenActivity.java
+++ trunk/src/com/groupagendas/groupagenda/contacts/importer/RequestTokenActivity.java
@@ -1,6 +1,9 @@
 package com.groupagendas.groupagenda.contacts.importer;
 
 import java.net.URLEncoder;
+import java.util.concurrent.ExecutionException;
+
+import com.groupagendas.groupagenda.error.report.Reporter;
 
 import oauth.signpost.OAuth;
 import oauth.signpost.OAuthConsumer;
@@ -12,6 +15,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -36,9 +40,18 @@ public class RequestTokenActivity extends Activity {
     				C.AUTHORIZE_URL);
     	} catch (Exception e) {
     		Log.e(C.TAG, "Error creating consumer / provider",e);
+			Reporter.reportError(this.getClass().toString(), Thread.currentThread().getStackTrace()[2].getMethodName().toString(), e.getMessage());
     	}
 
-    	getRequestToken();
+    	try {
+			new getRequestTokenTask().execute((Object) this).get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
 	
@@ -50,43 +63,105 @@ public class RequestTokenActivity extends Activity {
 		if (uri != null && uri.getScheme().equals(C.OAUTH_CALLBACK_SCHEME)) {
 			Log.i(C.TAG, "Callback received : " + uri);
 			Log.i(C.TAG, "Retrieving Access Token");
-			getAccessToken(uri);
-		}
-	}
-	
-	private void getRequestToken() {
-		try {
-			Log.d(C.TAG, "getRequestToken() called");
-			String url = provider.retrieveRequestToken(consumer, C.OAUTH_CALLBACK_URL);
-			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url)).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_FROM_BACKGROUND);
-			this.startActivity(intent);
 			
-		} catch (Exception e) {
-			Log.e(C.TAG, "Error retrieving request token", e);
+			Object[] params = {(Object) uri, (Object) this};
+			try {
+				new getAccessTokenTask().execute(params).get();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 	
-	private void getAccessToken(Uri uri) {
-		final String oauth_verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
-		try {
-			provider.retrieveAccessToken(consumer, oauth_verifier);
+//	private void getRequestToken() {
+//		try {
+//			Log.d(C.TAG, "getRequestToken() called");
+//			String url = provider.retrieveRequestToken(consumer, C.OAUTH_CALLBACK_URL);
+//			Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url)).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_FROM_BACKGROUND);
+//			this.startActivity(intent);
+//			
+//		} catch (Exception e) {
+//			Log.e(C.TAG, "Error retrieving request token", e);
+//		}
+//	}
+	
+//	private void getAccessToken(Uri uri) {
+//		final String oauth_verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
+//		try {
+//			provider.retrieveAccessToken(consumer, oauth_verifier);
+//
+//			final Editor edit = prefs.edit();
+//			edit.putString(OAuth.OAUTH_TOKEN, consumer.getToken());
+//			edit.putString(OAuth.OAUTH_TOKEN_SECRET, consumer.getTokenSecret());
+//			edit.commit();
+//			
+//			String token = prefs.getString(OAuth.OAUTH_TOKEN, "");
+//			String secret = prefs.getString(OAuth.OAUTH_TOKEN_SECRET, "");
+//			
+//			consumer.setTokenWithSecret(token, secret);
+//			this.startActivity(new Intent(this, ImportActivity.class));
+//
+//			Log.i(C.TAG, "Access Token Retrieved");
+//			
+//		} catch (Exception e) {
+//			Log.e(C.TAG, "Access Token Retrieval Error", e);
+//		}
+//	}
+	
+	
+	
+	private class getRequestTokenTask extends AsyncTask<Object, Void, Void> {
+		
+		@Override
+		protected Void doInBackground(Object... objects) {
+			try {
+				Log.d(C.TAG, "getRequestToken() called");
+				String url = provider.retrieveRequestToken(consumer, C.OAUTH_CALLBACK_URL);
+				Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url)).setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_FROM_BACKGROUND);
+				RequestTokenActivity rta = (RequestTokenActivity) objects[0];
+				rta.startActivity(intent);
+			} catch (Exception e) {
+				Log.e(C.TAG, "Error retrieving request token", e);
+				Reporter.reportError(this.getClass().toString(), Thread.currentThread().getStackTrace()[2].getMethodName().toString(), e.getMessage());
+			}
+			return null;
+		}
+	}
+	
+	
+	private class getAccessTokenTask extends AsyncTask<Object, Void, Void> {
 
-			final Editor edit = prefs.edit();
-			edit.putString(OAuth.OAUTH_TOKEN, consumer.getToken());
-			edit.putString(OAuth.OAUTH_TOKEN_SECRET, consumer.getTokenSecret());
-			edit.commit();
+		@Override
+		protected Void doInBackground(Object... objects) {
+			Uri uri = (Uri) objects[0];
+			RequestTokenActivity rta = (RequestTokenActivity) objects[1];
 			
-			String token = prefs.getString(OAuth.OAUTH_TOKEN, "");
-			String secret = prefs.getString(OAuth.OAUTH_TOKEN_SECRET, "");
-			
-			consumer.setTokenWithSecret(token, secret);
-			this.startActivity(new Intent(this, ImportActivity.class));
+			final String oauth_verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
+			try {
+				provider.retrieveAccessToken(consumer, oauth_verifier);
 
-			Log.i(C.TAG, "Access Token Retrieved");
-			
-		} catch (Exception e) {
-			Log.e(C.TAG, "Access Token Retrieval Error", e);
+				final Editor edit = prefs.edit();
+				edit.putString(OAuth.OAUTH_TOKEN, consumer.getToken());
+				edit.putString(OAuth.OAUTH_TOKEN_SECRET, consumer.getTokenSecret());
+				edit.commit();
+				
+				String token = prefs.getString(OAuth.OAUTH_TOKEN, "");
+				String secret = prefs.getString(OAuth.OAUTH_TOKEN_SECRET, "");
+				
+				consumer.setTokenWithSecret(token, secret);
+				rta.startActivity(new Intent(rta, ImportActivity.class));
+
+				Log.i(C.TAG, "Access Token Retrieved");
+				
+			} catch (Exception e) {
+				Log.e(C.TAG, "Access Token Retrieval Error", e);
+				Reporter.reportError(this.getClass().toString(), Thread.currentThread().getStackTrace()[2].getMethodName().toString(), e.getMessage());
+			}
+			return null;
 		}
 	}
-	
 }
