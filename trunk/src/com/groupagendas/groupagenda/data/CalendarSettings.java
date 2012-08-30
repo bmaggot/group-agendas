831224fa9bb865d51ea43f3c453214e6f7ee59df
Justinas.Marcinka@gmail.com <Justinas.Marcinka@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-08-30 10:13:02 +0000

    Calendar settings now is interface for safe setting and getting all calendar settings in account.

--- trunk/src/com/groupagendas/groupagenda/data/CalendarSettings.java
+++ trunk/src/com/groupagendas/groupagenda/data/CalendarSettings.java
@@ -1,46 +1,112 @@
+/**
+ * This class acts as an interface to account stored in data class. It is responsible for safe calendar settings setting and getting.
+ * @author justinas.marcinka@gmail.com
+ */
 package com.groupagendas.groupagenda.data;
+
+
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import com.groupagendas.groupagenda.account.Account;
+
 public class CalendarSettings {
 	
 	public static final int[] DEFAULT_WEEKENDS = {1,7};
-	private static  int[] weekends; //TODO remove this hardcode. should be set when loading account.
 	
 	public static final String DEFAULT_DATE_FORMAT = "yyyy-MM-dd";
+	/**
+	 * date format that is currently in use by date formatter. It may differ date format set on account.
+	 */
 	private static String dateFormat;
 	
-	public static final int DEFAULT_FIRST_DAY_OF_WEEK = Calendar.MONDAY;
-	private static int firstDayOfWeek = 0;
+	private static final int DEFAULT_FIRST_DAY_OF_WEEK = Calendar.MONDAY;
 	
-	public static SimpleDateFormat dateFormatter = new SimpleDateFormat(DEFAULT_DATE_FORMAT);
+	private static final String DEFAULT_SETTING_DEFAULT_VIEW = "m"; //MONTH VIEW
+
+	private static final boolean DEFAULT_AM_PM_SETTING = false;
+	
+	private static SimpleDateFormat dateFormatter = new SimpleDateFormat(DEFAULT_DATE_FORMAT);
 
 	public static int[] getWeekends() {
-		if (weekends == null) weekends = DEFAULT_WEEKENDS;
-		return weekends;
-	}
+//		TODO get weekends from account settings
+		return DEFAULT_WEEKENDS;
+	}	
 
-	public static void setWeekends(int[] weekends) {
-		CalendarSettings.weekends = weekends;
-	}
-
+	public static boolean isUsing_AM_PM() {
+		if (Data.getAccount() == null) return DEFAULT_AM_PM_SETTING;
+		return (Data.getAccount().setting_ampm == 1);
+	}	
 	
+	public static void setUsing_AM_PM(boolean bool) {
+		Account acc = Data.getAccount();
+		if (acc != null) {
+			if (bool){
+				acc.setting_ampm = 1;
+			}
+		}
+	}	
 
 	public static void setDateFormat(String date_format) {
-		dateFormatter = new SimpleDateFormat(date_format);
-		CalendarSettings.dateFormat = date_format;
+		Account account = Data.getAccount();
+		if (account != null){
+			account.setting_date_format = date_format;
+		}
 	}
 
 	public static String getDateFormat() {
-		if (dateFormat == null) dateFormat = DEFAULT_DATE_FORMAT;
+		Account account = Data.getAccount();
+		if (account != null) {
+			if (account.setting_date_format != null) {
+				if (!account.setting_date_format.equalsIgnoreCase("null")) {
+					dateFormat = account.setting_date_format;
+					return dateFormat;
+				}
+			}
+		}
+		dateFormat = DEFAULT_DATE_FORMAT;
 		return dateFormat;
+	}
+	
+	/**
+	 * Method that returns date formatter according to current account selected date format.
+	 * It is mainly used by Utils method formatCalendar.
+	 * @author justinas.marcinka@gmail.com
+	 * @return date formatter
+	 * @see com.groupagendas.groupagenda.utils.Utils
+	 */
+	public static SimpleDateFormat getDateFormatter() {
+		Account account = Data.getAccount();	
+		if (account.setting_date_format != null){
+			if (!account.setting_date_format.equalsIgnoreCase("null")){
+				if (!account.setting_date_format.equalsIgnoreCase(dateFormat)){
+					dateFormat = account.setting_date_format;
+					dateFormatter = new SimpleDateFormat(dateFormat);
+				}
+			}
+		}
+		return dateFormatter;
 	}
 
 	public static int getFirstDayofWeek() {
-		if (firstDayOfWeek == 0) firstDayOfWeek = DEFAULT_FIRST_DAY_OF_WEEK;
-		return firstDayOfWeek;
+//		TODO get first day of week from account
+		return DEFAULT_FIRST_DAY_OF_WEEK;
 	}
+
+	public static String getDefaultView() {
+		if (Data.getAccount() != null) {
+			String dw = Data.getAccount().setting_default_view;
+			if (dw == null) {
+				dw = DEFAULT_DATE_FORMAT;
+			} else if (dw.equalsIgnoreCase("null")) {
+				dw = DEFAULT_DATE_FORMAT;
+			}
+			return dw;
+		}
+		return DEFAULT_DATE_FORMAT;
+	}
+
 	
 	
 	
