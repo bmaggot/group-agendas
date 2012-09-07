a38c38aa79afb216461bb658ec3ca139a92a95d8
interfectos@gmail.com <interfectos@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-09-07 08:13:47 +0000

    https://trello.com/card/add-contact-to-your-list-based-on-joined-events-without-details/4fcf32aa9351b2603267d4f4/24 invite to contacts from eventedit.

--- trunk/src/com/groupagendas/groupagenda/events/Invited.java
+++ trunk/src/com/groupagendas/groupagenda/events/Invited.java
@@ -5,8 +5,11 @@ public class Invited extends Object{
 	public int my_contact_id;
 	
 	public String name;
+	public int gcid;
+	public int guid;
 	public String email;
 	public String status;
 	public boolean me = false;
 	public int contactId;
+	public boolean inMyList;
 }
