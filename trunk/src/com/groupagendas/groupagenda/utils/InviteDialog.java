0471d15bd6d736057a4e8ce0d808a63d983dc7e9
interfectos@gmail.com <interfectos@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-09-10 12:29:18 +0000

    invite new contact from event

--- trunk/src/com/groupagendas/groupagenda/utils/InviteDialog.java
+++ trunk/src/com/groupagendas/groupagenda/utils/InviteDialog.java
@@ -1,11 +1,24 @@
 package com.groupagendas.groupagenda.utils;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.Activity;
 import android.app.Dialog;
+import android.os.AsyncTask;
 import android.view.View;
 import android.widget.Button;
 
 import com.groupagendas.groupagenda.R;
+import com.groupagendas.groupagenda.data.Data;
+import com.groupagendas.groupagenda.data.DataManagement;
+import com.groupagendas.groupagenda.data.OfflineData;
+import com.groupagendas.groupagenda.events.Invited;
 
 public class InviteDialog extends Dialog {
 
@@ -13,16 +26,37 @@ public class InviteDialog extends Dialog {
 	private Button inviteAndRequest;
 	private Button justInvite;
 	private Button dontInvite;
+	private Invited invited;
+	private boolean req = false;
 
-	public InviteDialog(Activity context, int styleResId, String name) {
+	public InviteDialog(Activity context, int styleResId, final Invited invited) {
 		super(context, styleResId);
 		this.context = context;
+		this.invited = invited;
 		this.setContentView(R.layout.contacts_invite);
-		this.setTitle(context.getResources().getString(R.string.do_you_want_to_save_contact) + " " + name + " "
+		this.setTitle(context.getResources().getString(R.string.do_you_want_to_save_contact) + " " + invited.name + " "
 				+ context.getResources().getString(R.string.to_your_list));
 
 		inviteAndRequest = (Button) findViewById(R.id.contact_invite_and_request);
+		inviteAndRequest.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				req = true;
+				new Invite().execute();
+				dismiss();
+			}
+		});
 		justInvite = (Button) findViewById(R.id.contact_only_invite);
+		justInvite.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				req = false;
+				new Invite().execute();
+				dismiss();
+			}
+		});
 		dontInvite = (Button) findViewById(R.id.contact_dont_invite);
 
 		dontInvite.setOnClickListener(new View.OnClickListener() {
@@ -33,5 +67,32 @@ public class InviteDialog extends Dialog {
 			}
 		});
 	}
+	public class Invite extends AsyncTask<Void, Void, Void>{
 
+		@Override
+		protected Void doInBackground(Void... params) {
+			try {
+				HttpClient hc = new DefaultHttpClient();
+				HttpPost post = new HttpPost(Data.getServerUrl() + "mobile/contact_copy");
+
+				MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+				reqEntity.addPart("token", new StringBody(Data.getToken()));
+				reqEntity.addPart("guid", new StringBody(String.valueOf(invited.guid)));
+				if(req){
+					reqEntity.addPart("req_details", new StringBody("1"));
+				}
+				post.setEntity(reqEntity);
+				if (DataManagement.networkAvailable) {
+					HttpResponse rp = hc.execute(post);
+				} else {
+					OfflineData uplooad = new OfflineData("mobile/contact_copy", reqEntity);
+					Data.getUnuploadedData().add(uplooad);
+				}
+			} catch (Exception e) {
+
+			}
+			return null;
+		}
+		
+	}
 }
