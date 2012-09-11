34e4ae2dd6b1607078722ee93b5e2aa207b94430
Justinas.Marcinka <Justinas.Marcinka@gmail.com>
2012-09-11 13:10:57 +0000

    renamed class, also modified compare algorithm: if first event starts earlier, return 1, if events start time is equal, if first event ends later, return 1

---
+++ src/com/groupagendas/groupagenda/utils/EventTimeComparator.java
@@ -0,0 +1,22 @@
+package com.groupagendas.groupagenda.utils;
+
+import java.util.Calendar;
+import java.util.Comparator;
+
+import com.groupagendas.groupagenda.data.DataManagement;
+import com.groupagendas.groupagenda.events.Event;
+
+public class EventTimeComparator implements Comparator<Event> {
+
+	@Override
+	public int compare(Event event1, Event event2) {
+		if (event1.startCalendar.before(event2.startCalendar)) return -1;
+		if (event2.startCalendar.before(event1.startCalendar)) return 1;
+		if (event1.endCalendar.after(event2.endCalendar)) return -1;
+		if (event2.endCalendar.after(event1.endCalendar)) return 1;
+		return 0;
+	}
+
+	
+
+}
