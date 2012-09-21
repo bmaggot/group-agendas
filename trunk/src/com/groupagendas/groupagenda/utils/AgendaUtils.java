04962e3272ed44ea274b6d3217bc21ea7dcaa77b
Justinas.Marcinka@gmail.com <Justinas.Marcinka@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-09-21 15:16:52 +0000

    removed use of fields: Event.time Event.time_start Event.my_time_start Event.time_end Event.my_time_end

--- trunk/src/com/groupagendas/groupagenda/utils/AgendaUtils.java
+++ trunk/src/com/groupagendas/groupagenda/utils/AgendaUtils.java
@@ -5,14 +5,12 @@ import java.util.Calendar;
 
 import android.content.Context;
 
-import com.groupagendas.groupagenda.data.DataManagement;
 import com.groupagendas.groupagenda.events.Event;
 
 public class AgendaUtils {
 	public static int newInvites = 0;
 	public static ArrayList<Event> getActualEvents(Context context, ArrayList<Event> events) {
 		ArrayList<Event> actual_events = new ArrayList<Event>();
-		String date_format = DataManagement.SERVER_TIMESTAMP_FORMAT;
 		Calendar now = Calendar.getInstance();
 		now.setTimeInMillis(System.currentTimeMillis());
 		Calendar calendar_end = null;
@@ -22,8 +20,8 @@ public class AgendaUtils {
 		
 		for (int i = 0, l = events.size(); i < l; i++) {
 			event = events.get(i);
-			if (!event.my_time_end.equals("null")) {
-				calendar_end = Utils.stringToCalendar(event.my_time_end, event.timezone, date_format);
+			if (event.getEndCalendar() != null) {
+				calendar_end = event.getEndCalendar();
 
 				if (calendar_end.after(now)) {
 					if(event.status == 4){
