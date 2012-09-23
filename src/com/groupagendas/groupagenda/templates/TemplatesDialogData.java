03636927cc317917c043858853e1387c4f5ec0a9
meska.lt <meska.lt@gmail.com>
2012-09-23 14:12:04 +0000

    Data class for TemplateDialog.

---
+++ src/com/groupagendas/groupagenda/templates/TemplatesDialogData.java
@@ -0,0 +1,22 @@
+package com.groupagendas.groupagenda.templates;
+
+public class TemplatesDialogData {
+	String id;
+	String title;
+	
+	public void setID (String id) {
+		this.id = id;
+	}
+	
+	public String getID () {
+		return id;
+	}
+	
+	public void setTitle (String title) {
+		this.title = title;
+	}
+	
+	public String getTitle () {
+		return title;
+	}
+}
