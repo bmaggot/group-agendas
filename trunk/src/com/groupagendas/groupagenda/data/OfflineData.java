dd4b27a4f6ed88ffbdd050d53f43546ee1d16662
meska.lt@gmail.com <meska.lt@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-09-29 16:23:17 +0000

    Updates updates updates... Documentation pending.

--- trunk/src/com/groupagendas/groupagenda/data/OfflineData.java
+++ trunk/src/com/groupagendas/groupagenda/data/OfflineData.java
@@ -1,21 +1,37 @@
 package com.groupagendas.groupagenda.data;
 
+import java.util.Calendar;
+
 import org.apache.http.entity.mime.MultipartEntity;
+
+import com.groupagendas.groupagenda.utils.Utils;
 
 public class OfflineData {
 	private String location = "";
 	private MultipartEntity unuploadedEntity = new MultipartEntity();
+	private long created;
 	
-	public OfflineData (String location, MultipartEntity unuploadedEntity) {
+	public OfflineData(String location, MultipartEntity unuploadedEntity) {
 		this.location = location;
 		this.unuploadedEntity = unuploadedEntity;
+		this.created = Utils.millisToUnixTimestamp(Calendar.getInstance().getTimeInMillis());
 	}
 	
-	public String getLocation () {
-		return this.location;
+	public OfflineData(String location, MultipartEntity unuploadedEntity, long created) {
+		this.location = location;
+		this.unuploadedEntity = unuploadedEntity;
+		this.created = created;
+	}
+
+	public String getLocation() {
+		return location;
 	}
 	
-	public MultipartEntity getRequest () {
-		return this.unuploadedEntity;
+	public MultipartEntity getRequest() {
+		return unuploadedEntity;
+	}
+	
+	public long getCreated() {
+		return created;
 	}
 }
