019efc759768cc3fa20fbe057340d8004e5808bb
Interfectos@gmail.com <Interfectos@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-10-08 09:33:37 +0000

    rewritten push

--- trunk/src/com/groupagendas/groupagenda/settings/SettingsActivity.java
+++ trunk/src/com/groupagendas/groupagenda/settings/SettingsActivity.java
@@ -1,5 +1,13 @@
 package com.groupagendas.groupagenda.settings;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import android.app.ListActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -11,7 +19,11 @@ import android.widget.ListView;
 
 import com.groupagendas.groupagenda.GroupAgendasActivity;
 import com.groupagendas.groupagenda.R;
+import com.groupagendas.groupagenda.account.Account;
 import com.groupagendas.groupagenda.account.AccountActivity;
+import com.groupagendas.groupagenda.data.Data;
+import com.groupagendas.groupagenda.data.DataManagement;
+import com.groupagendas.groupagenda.error.report.Reporter;
 
 public class SettingsActivity extends ListActivity{
 	@Override
@@ -73,6 +85,7 @@ public class SettingsActivity extends ListActivity{
 	}
 	
 	public void logout(View v){
+		this.unsubscribeFromPush();
 		SharedPreferences prefs = getSharedPreferences("LATEST_CREDENTIALS", MODE_PRIVATE);
 		Editor prefEditor = prefs.edit();
 		
@@ -85,6 +98,28 @@ public class SettingsActivity extends ListActivity{
 		Intent intent = new Intent(SettingsActivity.this, GroupAgendasActivity.class);
 		intent.setFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP );
 		startActivity(intent);
+	}
+	
+	public void unsubscribeFromPush(){
+		try {
+
+			HttpClient hc = new DefaultHttpClient();
+			HttpPost post = new HttpPost(Data.getServerUrl() + "mobile/push/unsubscribe");
+
+			MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+
+			Account account = new Account(this);
+			reqEntity.addPart("device_uuid", new StringBody(account.getPushId()));
+
+			post.setEntity(reqEntity);
+
+			@SuppressWarnings("unused")
+			HttpResponse rp = hc.execute(post);
+
+		} catch (Exception ex) {
+			Reporter.reportError(DataManagement.class.toString(), Thread.currentThread().getStackTrace()[2].getMethodName().toString(),
+					ex.getMessage());
+		}
 	}
 	
 }
