d671ce2393ebd5f97a3a9a29c3b5229beab039b5
Interfectos@gmail.com <Interfectos@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-10-10 15:01:23 +0000

    chat all most done

--- trunk/src/com/groupagendas/groupagenda/calendar/adapters/ChatMessageAdapter.java
+++ trunk/src/com/groupagendas/groupagenda/calendar/adapters/ChatMessageAdapter.java
@@ -12,42 +12,56 @@ import android.widget.Toast;
 
 import com.groupagendas.groupagenda.R;
 import com.groupagendas.groupagenda.account.Account;
+import com.groupagendas.groupagenda.chat.ChatMessageActivity;
 import com.groupagendas.groupagenda.chat.ChatMessageObject;
 import com.groupagendas.groupagenda.data.ChatManagement;
+import com.groupagendas.groupagenda.utils.DateTimeUtils;
+import com.groupagendas.groupagenda.utils.Utils;
 
 public class ChatMessageAdapter extends AbstractAdapter<ChatMessageObject> {
 
-	public ChatMessageAdapter(Context context, List<ChatMessageObject> list) {
+	ChatMessageActivity context;
+	
+	public ChatMessageAdapter(ChatMessageActivity context, List<ChatMessageObject> list) {
 		super(context, list);
+		this.context = context;
 	}
 
 	@Override
 	public View getView(int i, View view, ViewGroup viewGroup) {
 		if (view == null) {
-			view = mInflater.inflate(R.layout.chat_message, null);
+			view = mInflater.inflate(R.layout.chat_message, viewGroup, false);
 		}
 
 		final ChatMessageObject chatMessage = (ChatMessageObject) this.getItem(i);
 		if (!chatMessage.isDeleted()) {
-			Account account = new Account();
+			Account account = new Account(context);
 			TextView messageBody = (TextView) view.findViewById(R.id.chat_message_body);
 			messageBody.setText(chatMessage.getMessage());
 			TextView chatTime = (TextView) view.findViewById(R.id.chat_message_time);
 			Calendar calendar = Calendar.getInstance();
-			calendar.setTimeInMillis(chatMessage.getCreated());
-			chatTime.setText(calendar.getTime().toString());
+			calendar.setTimeInMillis(Utils.unixTimestampToMilis(chatMessage.getCreated()));
+			DateTimeUtils dtUtils = new DateTimeUtils(getContext());
+			chatTime.setText(dtUtils.formatDateTime(calendar));
 			if (chatMessage.getUserId() == account.getUser_id()) {
 				view.findViewById(R.id.kubiks).setVisibility(View.VISIBLE);
-
 				TextView iksiuks = (TextView) view.findViewById(R.id.chat_message_delete);
+				iksiuks.setClickable(true);
 				iksiuks.setVisibility(View.VISIBLE);
+				view.setTag(true);
 				iksiuks.setOnClickListener(new OnClickListener() {
+					
 					@Override
-					public void onClick(View v) {
-						Toast.makeText(getContext(), R.string.delete_chat_message, Toast.LENGTH_SHORT).show();
-						ChatManagement.removeChatMessage(getContext(), chatMessage.getMessageId());
+					public void onClick(View arg0) {
+						context.deleteMessage(chatMessage.getMessageId(), chatMessage);
 					}
 				});
+			} else {
+				view.findViewById(R.id.kubiks).setVisibility(View.GONE);
+				TextView iksiuks = (TextView) view.findViewById(R.id.chat_message_delete);
+				iksiuks.setClickable(false);
+				iksiuks.setVisibility(View.GONE);
+				view.setTag(false);
 			}
 		}
 
--- trunk/src/com/groupagendas/groupagenda/chat/ChatMessageActivity.java
+++ trunk/src/com/groupagendas/groupagenda/chat/ChatMessageActivity.java
@@ -23,10 +23,13 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.groupagendas.groupagenda.R;
 import com.groupagendas.groupagenda.calendar.adapters.ChatMessageAdapter;
@@ -42,6 +45,7 @@ public class ChatMessageActivity extends Activity {
 	Button chatSend;
 	ChatMessageAdapter adapter;
 	ListView chat_message_list;
+	ArrayList<ChatMessageObject> chatMessages;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -55,14 +59,12 @@ public class ChatMessageActivity extends Activity {
 
 		event_id = getIntent().getIntExtra("event_id", 0);
 
-		ArrayList<ChatMessageObject> chatMessages = new ArrayList<ChatMessageObject>();
 		Object[] params = { this, event_id };
 		new GetChatMessagesForEventFromRemoteDb().execute(params);
-		adapter = new ChatMessageAdapter(ChatMessageActivity.this, chatMessages);
-		adapter.setList(chatMessages);
-		adapter.notifyDataSetChanged();
 
+		adapter = new ChatMessageAdapter(ChatMessageActivity.this, chatMessages);
 		chat_message_list = (ListView) findViewById(R.id.chat_message_list);
+		chat_message_list.setAdapter(adapter);
 		chatInput = (EditText) findViewById(R.id.chat_input);
 		chatSend = (Button) findViewById(R.id.chat_send);
 
@@ -71,7 +73,7 @@ public class ChatMessageActivity extends Activity {
 			@Override
 			public void onClick(View arg0) {
 				String message = chatInput.getText().toString();
-				Object[] params = { this, message, event_id };
+				Object[] params = { ChatMessageActivity.this, message, event_id };
 				new PostChatMessage().execute(params);
 				chatInput.setText("");
 				InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -88,41 +90,14 @@ public class ChatMessageActivity extends Activity {
 		protected Void doInBackground(Object... params) {
 			Context context = (Context) params[0];
 			int eventId = (Integer) params[1];
-			boolean success = false;
-			HttpClient hc = new DefaultHttpClient();
-			HttpPost post = new HttpPost(Data.getServerUrl() + "mobile/chat_get");
-			MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
-
-			try {
-				reqEntity.addPart(ChatManagement.TOKEN, new StringBody(Data.getToken()));
-				reqEntity.addPart("event_id", new StringBody(String.valueOf(eventId)));
-			} catch (UnsupportedEncodingException e1) {
-				e1.printStackTrace();
-			}
-
-			post.setEntity(reqEntity);
-			try {
-				HttpResponse rp = hc.execute(post);
-				if (rp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-					String resp = EntityUtils.toString(rp.getEntity());
-					if (resp != null) {
-						JSONObject object = new JSONObject(resp);
-						success = object.getBoolean("success");
-						if (success) {
-							JSONArray chatMessages = object.getJSONArray("items");
-							for (int i = 0, l = chatMessages.length(); i < l; i++) {
-								ChatManagement.insertChatMessageContentValueToLocalDb(context,
-										ChatManagement.makeChatMessageObjectContentValueFromJSON(chatMessages.getJSONObject(i)));
-							}
-						} else {
-							Toast.makeText(context, object.getString("error"), Toast.LENGTH_LONG);
-						}
-					}
-				}
-			} catch (Exception e) {
-				Log.e("getChatMessagesForEventFromRemoteDb(Context context, int eventId " + eventId + ")", e.getMessage());
-			}
+			chatMessages = ChatManagement.getChatMessagesForEventFromRemoteDb(eventId, chatMessages, context);
 			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			adapter.setList(chatMessages);
+			adapter.notifyDataSetChanged();
 		}
 
 	}
@@ -134,48 +109,40 @@ public class ChatMessageActivity extends Activity {
 			Context context = (Context) params[0];
 			String message = (String) params[1];
 			int eventId = (Integer) params[2];
-			try {
-				boolean success = false;
-				HttpClient hc = new DefaultHttpClient();
-				HttpPost post = new HttpPost(Data.getServerUrl() + "mobile/chat_post");
-
-				MultipartEntity reqEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
-
-				reqEntity.addPart(ChatManagement.TOKEN, new StringBody(Data.getToken()));
-				reqEntity.addPart("event_id", new StringBody(String.valueOf(eventId)));
-				if (message == null) {
-					reqEntity.addPart("message", new StringBody(String.valueOf("")));
-				} else {
-					reqEntity.addPart("message", new StringBody(String.valueOf(message)));
-				}
-
-				post.setEntity(reqEntity);
-				HttpResponse rp = null;
-				if (DataManagement.networkAvailable) {
-					rp = hc.execute(post);
-					if (rp.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-						String resp = EntityUtils.toString(rp.getEntity());
-						if (resp != null) {
-							JSONObject object = new JSONObject(resp);
-							success = object.getBoolean("success");
-							if (success) {
-								ChatManagement.insertChatMessageContentValueToLocalDb(context,
-										ChatManagement.makeChatMessageObjectContentValueFromJSON(object.getJSONObject("message")));
-							} else {
-								Toast.makeText(context, object.getString("error"), Toast.LENGTH_LONG);
-							}
-						}
-					}
-				} else {
-					OfflineData uplooad = new OfflineData("mobile/chat_post", reqEntity);
-					Data.getUnuploadedData().add(uplooad);
-				}
-			} catch (Exception e) {
-				Log.e("postChatMessage(Context context, message " + message + ", event id " + eventId + ")", e.getMessage());
-			}
+			chatMessages.add(ChatManagement.postChatMessage(eventId, message, context));
 			return null;
 		}
 
+		@Override
+		protected void onPostExecute(Void result) {
+			adapter.notifyDataSetChanged();
+		}
+
+	}
+
+	public class RemoveChatMessageFromRemoteDb extends AsyncTask<Object, ChatMessageObject, ChatMessageObject> {
+
+		@Override
+		protected ChatMessageObject doInBackground(Object... params) {
+			Context context = (Context) params[0];
+			int messageId = (Integer) params[1];
+			ChatMessageObject chatMessageObject = (ChatMessageObject) params[2];
+			ChatManagement.removeChatMessage(context, messageId);
+			return chatMessageObject;
+		}
+
+		@Override
+		protected void onPostExecute(ChatMessageObject result) {
+			chatMessages.remove(result);
+			adapter.notifyDataSetChanged();
+		}
+
+	}
+
+	public void deleteMessage(int message_id, ChatMessageObject chatMessageObject) {
+		Object[] params = {ChatMessageActivity.this, message_id, chatMessageObject};
+		new RemoveChatMessageFromRemoteDb().execute(params);
+		
 	}
 
 }
