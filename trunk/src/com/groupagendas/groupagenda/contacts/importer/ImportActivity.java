84ba44c5b2cf2890035cbdf781eadd3dda7faed9
Audrius6@gmail.com <Audrius6@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-10-24 11:41:37 +0000

    Major phone contact import changes

--- trunk/src/com/groupagendas/groupagenda/contacts/importer/ImportActivity.java
+++ trunk/src/com/groupagendas/groupagenda/contacts/importer/ImportActivity.java
@@ -2,6 +2,7 @@ package com.groupagendas.groupagenda.contacts.importer;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.AsyncTask;
@@ -109,35 +110,77 @@ public class ImportActivity extends Activity {
 		protected Void doInBackground(Activity... params) {
 			// import sim contacts
 			Activity activity = params[0];
-			Cursor phones = activity.getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
-			while (phones.moveToNext()) {
-				String displayName = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
-				String[] nameArray;
-				String firstName = "";
-				String lastName = "";
-				String phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-				Contact phoneContact = new Contact();
-				if (displayName.matches("[0-9,A-Z,a-z]*\\s[0-9,A-Z,a-z]*")) {
-					nameArray = displayName.split("\\s");
-					firstName = nameArray[0];
-					if (nameArray.length <= 2) {
-						lastName = nameArray[1];
-					} else if (nameArray.length > 2) {
-						for (int i = 1; i < nameArray.length; i++) {
-							lastName = lastName + nameArray[i];
-						}
-					}
-				} else {
-					firstName = displayName;
-					lastName = "";
-				}
+			String id = "";
 
-				phoneContact.name = firstName;
-				phoneContact.lastname = lastName;
-				phoneContact.phone1 = phoneNumber;
-				ContactManagement.insertContact(ImportActivity.this, phoneContact);
+			ContentResolver cr = getContentResolver();
+			Cursor cur = cr.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+			if (cur.moveToFirst()) {
+				while (!cur.isAfterLast()) {
+					Contact phoneContact2 = new Contact();
+					id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+					Cursor cur1 = cr.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
+							ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = " + id, null, null);
+
+					if (cur1.moveToFirst()) {
+						while (!cur1.isAfterLast()) {
+							String displayName = cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+							String[] nameArray;
+							String firstName = "";
+							String lastName = "";
+
+							if (displayName.matches("[0-9,A-Z,a-z]*\\s[0-9,A-Z,a-z]*")) {
+								nameArray = displayName.split("\\s");
+								firstName = nameArray[0];
+								if (nameArray.length <= 2) {
+									lastName = nameArray[1];
+								} else if (nameArray.length > 2) {
+									for (int i = 1; i < nameArray.length; i++) {
+										lastName = lastName + nameArray[i];
+									}
+								}
+							} else {
+								firstName = displayName;
+								lastName = "";
+							}
+
+							phoneContact2.name = firstName;
+							phoneContact2.lastname = lastName;
+							Cursor phone = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+									ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + id, null, null);
+							if (phone.moveToFirst()) {
+								while (!phone.isAfterLast()) {
+									phoneContact2.phone1 = phone.getString(phone
+											.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+									break;
+								}
+							}
+
+							phoneContact2.email = cur1.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.Email.DATA));
+							break;
+						}
+						cur1.close();
+					}
+
+					cur1 = getContentResolver().query(ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_URI, null,
+							ContactsContract.CommonDataKinds.StructuredPostal.CONTACT_ID + " = " + id, null, null);
+					if (cur1.moveToFirst()) {
+						while (!cur1.isAfterLast()) {
+							phoneContact2.city = cur1
+									.getString(cur1.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.CITY));
+							phoneContact2.street = cur1.getString(cur1
+									.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.STREET));
+							phoneContact2.zip = cur1.getString(cur1
+									.getColumnIndex(ContactsContract.CommonDataKinds.StructuredPostal.POSTCODE));
+							break;
+						}
+						cur1.close();
+					}
+					ContactManagement.insertContact(ImportActivity.this, phoneContact2);
+					cur.moveToNext();
+				}
 			}
-			phones.close();
+
 			// import phone contacts
 			Cursor phoneContacts = activity.getContentResolver()
 					.query(RawContacts.CONTENT_URI,
@@ -145,38 +188,40 @@ public class ImportActivity extends Activity {
 									ContactsContract.CommonDataKinds.Email._ID },
 							RawContacts.ACCOUNT_TYPE + " <> 'com.android.contacts.sim' " + " AND " + RawContacts.ACCOUNT_TYPE
 									+ " == 'com.google' ", null, null);
-			while (phoneContacts.moveToNext()) {
-				String displayName = phoneContacts.getString(phoneContacts.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-				String rawContactId = phoneContacts.getString(phoneContacts.getColumnIndex(RawContacts._ID));
-				String[] nameArray;
-				String firstName = "";
-				String lastName = "";
-				Contact phoneContact = new Contact();
-				if (displayName.matches("[0-9,A-Z,a-z]*\\s[0-9,A-Z,a-z]*")) {
-					nameArray = displayName.split("\\s");
-					firstName = nameArray[0];
-					if (nameArray.length <= 2) {
-						lastName = nameArray[1];
-					} else if (nameArray.length > 2) {
-						for (int i = 1; i < nameArray.length; i++) {
-							lastName = lastName + nameArray[i];
+			if (phoneContacts.moveToFirst()) {
+				do {
+					String displayName = phoneContacts.getString(phoneContacts.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+					String rawContactId = phoneContacts.getString(phoneContacts.getColumnIndex(RawContacts._ID));
+					String[] nameArray;
+					String firstName = "";
+					String lastName = "";
+					Contact phoneContact = new Contact();
+					if (displayName.matches("[0-9,A-Z,a-z]*\\s[0-9,A-Z,a-z]*")) {
+						nameArray = displayName.split("\\s");
+						firstName = nameArray[0];
+						if (nameArray.length <= 2) {
+							lastName = nameArray[1];
+						} else if (nameArray.length > 2) {
+							for (int i = 1; i < nameArray.length; i++) {
+								lastName = lastName + nameArray[i];
+							}
 						}
+					} else {
+						firstName = displayName;
+						lastName = "";
 					}
-				} else {
-					firstName = displayName;
-					lastName = "";
-				}
-				phoneContact.name = firstName;
-				if (!displayName.matches("[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})")) {
+					phoneContact.name = firstName;
+					if (!displayName.matches("[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})")) {
 
-				}
-				phoneContact.email = getContactEmail(rawContactId, (ImportActivity) activity, phoneContact);
-				phoneContact.phone1 = getContactPhoneNumber(rawContactId, (ImportActivity) activity, phoneContact);
-				phoneContact.lastname = lastName;
-				ContactManagement.insertContact(ImportActivity.this, phoneContact);
+					}
+					phoneContact.email = getContactEmail(rawContactId, (ImportActivity) activity, phoneContact);
+					phoneContact.phone1 = getContactPhoneNumber(rawContactId, (ImportActivity) activity, phoneContact);
+					phoneContact.lastname = lastName;
+					ContactManagement.insertContact(ImportActivity.this, phoneContact);
+				} while (phoneContacts.moveToNext());
 			}
 			phoneContacts.close();
-//			TODO update contacts list in local DB
+			// TODO update contacts list in local DB
 			return null;
 		}
 
