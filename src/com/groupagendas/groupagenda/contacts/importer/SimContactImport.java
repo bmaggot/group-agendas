5add8e11235ee389008dfdb85aa0f58700026d0f
Audrius6 <Audrius6@gmail.com>
2012-10-25 13:19:43 +0000

    Major sim contact import changes

--- src/com/groupagendas/groupagenda/contacts/importer/SimContactImport.java
+++ src/com/groupagendas/groupagenda/contacts/importer/SimContactImport.java
@@ -1,31 +1,37 @@
 package com.groupagendas.groupagenda.contacts.importer;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+import android.util.Log;
+
 import com.groupagendas.groupagenda.contacts.Contact;
 import com.groupagendas.groupagenda.data.ContactManagement;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.RawContacts;
-
 public class SimContactImport {
-	public static void ImportSimContacts(Context context){
-		Cursor phoneContacts = context.getContentResolver()
-				.query(RawContacts.CONTENT_URI,
-						new String[] { RawContacts._ID, RawContacts.ACCOUNT_TYPE, ContactsContract.Contacts.DISPLAY_NAME, 
-								ContactsContract.CommonDataKinds.Email._ID },
-						RawContacts.ACCOUNT_TYPE + " == 'com.android.contacts.sim' " + " AND " + RawContacts.ACCOUNT_TYPE
-								+ " != 'com.google' ", null, null);
-		if (phoneContacts.moveToFirst()) {
-			do {
-				String displayName = phoneContacts.getString(phoneContacts.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-				String rawContactId = phoneContacts.getString(phoneContacts.getColumnIndex(RawContacts._ID));
+	public static final String DISPLAY_NAME = "name";
+	public static final String NUMBER = "number";
+	public static final String URI = "content://icc/adn";
+
+	public static void importSimContacts(Context context) {
+
+		String simPhoneName = "";
+		String simPhoneNo = "";
+
+		Uri simUri = Uri.parse(URI);
+		Cursor cursorSim = context.getContentResolver().query(simUri, null, null, null, null);
+
+		if (cursorSim.moveToFirst()) {
+			while (!cursorSim.isAfterLast()) {
+				Contact simContact = new Contact();
+				simPhoneName = cursorSim.getString(cursorSim.getColumnIndex(DISPLAY_NAME));
 				String[] nameArray;
 				String firstName = "";
-				String lastName = ""; 
-				Contact phoneContact = new Contact();
-				if (displayName.matches("[0-9,A-Z,a-z]*\\s[0-9,A-Z,a-z]*")) {
-					nameArray = displayName.split("\\s");
+				String lastName = "";
+
+				if (simPhoneName.matches("[0-9,A-Z,a-z]*\\s[0-9,A-Z,a-z]*")) {
+					nameArray = simPhoneName.split("\\s");
 					firstName = nameArray[0];
 					if (nameArray.length <= 2) {
 						lastName = nameArray[1];
@@ -35,22 +41,26 @@ public class SimContactImport {
 						}
 					}
 				} else {
-					firstName = displayName;
+					firstName = simPhoneName;
 					lastName = "";
 				}
-				phoneContact.name = firstName;
-				if (!displayName.matches("[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})")) {
 
-				}
-				phoneContact.email = getContactEmail(rawContactId, context, phoneContact);
-				phoneContact.phone1 = getContactPhoneNumber(rawContactId, context, phoneContact);
-				phoneContact.lastname = lastName;
-				ContactManagement.insertContact(context, phoneContact);
-			} while (phoneContacts.moveToNext());
+				simPhoneNo = cursorSim.getString(cursorSim.getColumnIndex(NUMBER));
+				simPhoneNo.replaceAll("\\D", "");
+				simPhoneNo.replaceAll("&", "");
+				simPhoneName = simPhoneName.replace("|", "");
+				simContact.name = firstName;
+				simContact.lastname = lastName;
+				simContact.phone1 = simPhoneNo;
+				Log.i("SimContacts", simPhoneName);
+				Log.i("SimContactsNo", simPhoneNo);
+				ContactManagement.insertContact(context, simContact);
+				cursorSim.moveToNext();
+			}
 		}
-		phoneContacts.close();
+		cursorSim.close();
 	}
-	
+
 	public static String getContactEmail(String id, Context context, Contact contact) {
 		String email = "";
 		Cursor emailCur = context.getContentResolver().query(ContactsContract.CommonDataKinds.Email.CONTENT_URI, null,
