9274c0b26f9cf20dcf11f520c3a534e15d37e437
meska.lt <meska.lt@gmail.com>
2012-11-12 12:59:14 +0000

    Major adapter's event time displaying update (start+end/start+.../...+end/all day).

--- src/com/groupagendas/groupagenda/calendar/adapters/MonthAdapter.java
+++ src/com/groupagendas/groupagenda/calendar/adapters/MonthAdapter.java
@@ -1,7 +1,10 @@
 package com.groupagendas.groupagenda.calendar.adapters;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.TreeMap;
 
 import android.content.Context;
 import android.view.View;
@@ -13,19 +16,38 @@ import com.groupagendas.groupagenda.EventActivityOnClickListener;
 import com.groupagendas.groupagenda.R;
 import com.groupagendas.groupagenda.events.Event;
 import com.groupagendas.groupagenda.utils.DrawingUtils;
+import com.groupagendas.groupagenda.utils.TreeMapUtils;
+import com.groupagendas.groupagenda.utils.Utils;
 
 public class MonthAdapter extends AbstractAdapter<Event> {
 	
-	SimpleDateFormat timeFormat;
+	public static final String SERVER_TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
+	SimpleDateFormat timeFormat;
+	TreeMap<Calendar, ArrayList<Event>> sortedEvents;
+	Calendar selectedDate;
 
 	public MonthAdapter(Context context, List<Event> list) {
 		super(context, list);
 		setAMPM(false);
 	}
+	
 	public MonthAdapter(Context context, List<Event> list, boolean setAMPM) {
 		super(context, list);
 		setAMPM(setAMPM);
+	}
+	
+	public MonthAdapter(Context context, List<Event> list, boolean setAMPM, TreeMap<Calendar, ArrayList<Event>> sortedEvents) {
+		super(context, list);
+		setAMPM(setAMPM);
+		this.sortedEvents = sortedEvents;
+	}
+	
+	public MonthAdapter(Context context, List<Event> list, boolean setAMPM, TreeMap<Calendar, ArrayList<Event>> sortedEvents, Calendar selectedDate) {
+		super(context, list);
+		setAMPM(setAMPM);
+		this.sortedEvents = sortedEvents;
+		this.selectedDate = selectedDate;
 	}
 	
 	public void setAMPM(boolean usesAMPM) {
@@ -38,6 +60,12 @@ public class MonthAdapter extends AbstractAdapter<Event> {
 					R.string.time_format));
 		}
 	}
+	
+	public void setSelectedDate (Calendar date, TreeMap<Calendar, ArrayList<Event>> sortedEvents) {
+		this.selectedDate = date;
+		this.sortedEvents = sortedEvents; 
+		notifyDataSetChanged();
+	}
 
 	@Override
 	public View getView(int i, View view, ViewGroup viewGroup) {
@@ -48,23 +76,78 @@ public class MonthAdapter extends AbstractAdapter<Event> {
 		if (item instanceof Event){
 			Event event = (Event) item;
 			
-			ImageView colourBubble = (ImageView) view.findViewById(R.id.month_entry_color_placeholder);
-			colourBubble.setImageBitmap(DrawingUtils.getColoredRoundRectangle(context, 20, event.getDisplayColor(), true));
-			
-			TextView startTime = (TextView) view.findViewById(R.id.month_entry_start);
-			startTime.setText(timeFormat.format(event.getStartCalendar().getTime()));
-			
-			TextView endTime = (TextView) view.findViewById(R.id.month_entry_end);
-			endTime.setText(timeFormat.format(event.getEndCalendar().getTime()));
+			ArrayList<Event> events;
+			boolean isYesterday = false;
+			boolean isTomorrow = false;
 			
 			ImageView icon = (ImageView) view.findViewById(R.id.month_entry_icon_placeholder);
+			ImageView colourBubble = (ImageView) view.findViewById(R.id.month_entry_color_placeholder);
+			TextView startTime = (TextView) view.findViewById(R.id.month_entry_start);
+			TextView endTime = (TextView) view.findViewById(R.id.month_entry_end);
 			
+			String dayStr = new SimpleDateFormat("yyyy-MM-dd").format(selectedDate.getTime());
+			Calendar date = Utils.stringToCalendar(context, dayStr + " 00:00:00", SERVER_TIMESTAMP_FORMAT);
 			
-			 if (event.getIcon().length() <= 0){
-					icon.setVisibility(View.INVISIBLE);
-				}else{
-					icon.setImageResource(event.getIconId(getContext()));
+			colourBubble.setImageBitmap(DrawingUtils.getColoredRoundRectangle(context, 20, event.getDisplayColor(), true));
+
+			if (sortedEvents != null) {
+				date.add(Calendar.DAY_OF_YEAR, -1);
+				events = TreeMapUtils.getEventsFromTreemap(date, sortedEvents);
+				if (events != null) {
+					for (Event e : events) {
+						if (event.getEvent_id() == e.getEvent_id()) {
+							isYesterday = true;
+							break;
+						}
+					}
 				}
+				
+				date.add(Calendar.DAY_OF_YEAR, 2);
+				events = TreeMapUtils.getEventsFromTreemap(date, sortedEvents);
+				if (events != null) {
+					for (Event e : events) {
+						if (event.getEvent_id() == e.getEvent_id()) {
+							isTomorrow = true;
+							break;
+						}
+					}
+				}
+			}
+			
+			if (isYesterday && isTomorrow) {
+				startTime.setText(R.string.all_day);
+				endTime.setText("");
+				
+				endTime.setVisibility(View.GONE);
+			} else {
+				if (event.isBirthday()) {
+					startTime.setVisibility(View.INVISIBLE);
+					endTime.setVisibility(View.INVISIBLE);
+				} else {
+					startTime.setVisibility(View.VISIBLE);
+					endTime.setVisibility(View.VISIBLE);
+				}
+				if (isYesterday) {
+					startTime.setText(R.string.three_dots);
+				} else {
+					startTime.setText(timeFormat.format(event.getStartCalendar().getTime()));
+				}
+				
+				if (isTomorrow) {
+					endTime.setText(R.string.three_dots);
+				} else {
+					endTime.setText(timeFormat.format(event.getEndCalendar().getTime()));
+				}
+			}
+			
+//			startTime.setText(timeFormat.format(event.getStartCalendar().getTime()));
+//			endTime.setText(timeFormat.format(event.getEndCalendar().getTime()));
+			
+			if (event.getIcon().length() <= 0) {
+				icon.setVisibility(View.INVISIBLE);
+			}else{
+				icon.setImageResource(event.getIconId(getContext()));
+			}
 			
 			TextView title = (TextView) view.findViewById(R.id.month_entry_title);
 			title.setText(event.getTitle());
