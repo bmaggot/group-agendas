4f0a545d3dd1eae8be0aa9da575adbf739d7a9b5
Meska.LT@gmail.com <Meska.LT@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-11-21 16:10:05 +0000

    Disabled ignore of sideway swipe.

--- trunk/src/com/groupagendas/groupagenda/calendar/SwipeOnGestureListener.java
+++ trunk/src/com/groupagendas/groupagenda/calendar/SwipeOnGestureListener.java
@@ -1,36 +1,29 @@
 package com.groupagendas.groupagenda.calendar;
 
-
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 
 public class SwipeOnGestureListener extends SimpleOnGestureListener {
 
-	  private static final int SWIPE_MIN_DISTANCE = 50;
-	  private static final int SWIPE_MAX_OFF_PATH = 100;
-	  private static final int SWIPE_THRESHOLD_VELOCITY = 20;
-	  
-	  protected AbstractCalendarView parentView;
-	  
-	  public SwipeOnGestureListener (AbstractCalendarView parent){
-		  parentView = parent;
-	  }
-	  
+	private static final int SWIPE_MIN_DISTANCE = 50;
+	// private static final int SWIPE_MAX_OFF_PATH = 100;
+	private static final int SWIPE_THRESHOLD_VELOCITY = 20;
 
-		  
-	  
+	protected AbstractCalendarView parentView;
 
-	  @Override
-	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {  
-	   if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
-	    return false;
-	   if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE
-	     && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-		   parentView.goNext();
-	   } else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE
-	     && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
-		   parentView.goPrev();
-	   }
-	   return super.onFling(e1, e2, velocityX, velocityY);
-	  }
+	public SwipeOnGestureListener(AbstractCalendarView parent) {
+		parentView = parent;
 	}
+
+	@Override
+	public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+		// if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+		// return false;
+		if (e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+			parentView.goNext();
+		} else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+			parentView.goPrev();
+		}
+		return super.onFling(e1, e2, velocityX, velocityY);
+	}
+}
