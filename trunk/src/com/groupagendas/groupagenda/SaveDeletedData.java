ad929ad6fd9255c5d1444189b655a1e7ba78a637
Meska.LT@gmail.com <Meska.LT@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-11-29 13:18:31 +0000

    Major fix.

--- trunk/src/com/groupagendas/groupagenda/SaveDeletedData.java
+++ trunk/src/com/groupagendas/groupagenda/SaveDeletedData.java
@@ -3,9 +3,6 @@ package com.groupagendas.groupagenda;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
-import android.preference.PreferenceManager;
-import android.provider.Contacts.Settings;
-import android.provider.Contacts.SettingsColumns;
 
 public class SaveDeletedData {
 	SharedPreferences prefs;
@@ -27,30 +24,51 @@ public class SaveDeletedData {
 		addIdToPrefs(SDMetaData.DELETED_EVENTS, external_id);
 	}
 	
-	public void addContactForLaterDelete(int external_id){
-		addIdToPrefs(SDMetaData.DELETED_CONTACTS, external_id);
+	public boolean addContactForLaterDelete(int external_id){
+		String entry = prefs.getString(SaveDeletedData.SDMetaData.DELETED_CONTACTS, "");
+		
+		if (entry.length() == 0) {
+			entry += external_id;
+		} else {
+			entry += SDMetaData.SEPARATOR + external_id;
+		}
+			
+		prefsEditor.putString(SaveDeletedData.SDMetaData.DELETED_CONTACTS, entry);
+		return prefsEditor.commit();
 	}
 	
 	public void addGroupForLaterDelete(int external_id){
 		addIdToPrefs(SDMetaData.DELETED_GROUPS, external_id);
 	}
 	
-	private void addIdToPrefs(String columnName, int external_id) {
+	private boolean addIdToPrefs(String columnName, int external_id) {
 		String entry = prefs.getString(columnName, "");
-		if (entry.length() == 0){
-			entry+= external_id;
-		}else {
-			entry+= SDMetaData.SEPARATOR + external_id;
+		
+		if (entry.length() == 0) {
+			entry += external_id;
+		} else {
+			entry += SDMetaData.SEPARATOR + external_id;
 		}
 			
 		prefsEditor.putString(columnName, entry);
-		prefsEditor.commit();
+		return prefsEditor.commit();
 	}
 	
-	public void clear(){
+	public void clear(int state){
 	    SharedPreferences.Editor editor = prefs.edit();
 
-	    editor.clear();
+	    switch (state) {
+		case 1:
+			prefsEditor.putString(SaveDeletedData.SDMetaData.DELETED_CONTACTS, "");
+			break;
+		case 2:
+			prefsEditor.putString(SaveDeletedData.SDMetaData.DELETED_GROUPS, "");
+			break;
+		case 3:
+			prefsEditor.putString(SaveDeletedData.SDMetaData.DELETED_EVENTS, "");
+			break;
+		}
+	    
 	    editor.commit();	
 	}
 	
@@ -58,9 +76,11 @@ public class SaveDeletedData {
 	public String getDELETED_EVENTS() {
 		return prefs.getString(SaveDeletedData.SDMetaData.DELETED_EVENTS, "");
 	}
+	
 	public String getDELETED_CONTACTS() {
-		return prefs.getString(SaveDeletedData.SDMetaData.DELETED_CONTACTS,"");
+		return prefs.getString(SaveDeletedData.SDMetaData.DELETED_CONTACTS, "");
 	}
+	
 	public String getDELETED_GROUPS() {
 		return prefs.getString(SaveDeletedData.SDMetaData.DELETED_GROUPS, "");
 	}
