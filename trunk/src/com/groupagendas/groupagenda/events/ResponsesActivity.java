50c25a074d6c114a1f6420635531b212614d0295
Audrius6@gmail.com <Audrius6@gmail.com@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2012-12-03 14:37:32 +0000

    fixses with responses

--- trunk/src/com/groupagendas/groupagenda/events/ResponsesActivity.java
+++ trunk/src/com/groupagendas/groupagenda/events/ResponsesActivity.java
@@ -1,27 +1,58 @@
 package com.groupagendas.groupagenda.events;
 
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 
 import com.groupagendas.groupagenda.R;
+import com.groupagendas.groupagenda.account.Account;
 import com.groupagendas.groupagenda.calendar.adapters.ResponsesAdapter;
-import com.groupagendas.groupagenda.data.EventManagement;
 
 public class ResponsesActivity extends Activity {
-	
+
 	ResponsesAdapter adapter;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.responses_threads);
-		
+
 		ListView responses_thread_list = (ListView) findViewById(R.id.responses_list);
-		adapter = new ResponsesAdapter(ResponsesActivity.this, EventManagement.getResponsesFromRemoteDb(getApplicationContext()));
+
+		boolean success = false;
+		String error = null;
+		ArrayList<JSONObject> list = new ArrayList<JSONObject>();
+		Account acc = new Account(this);
+		try {
+			JSONObject object = new JSONObject(acc.getResponses());
+			success = object.getBoolean("success");
+
+			if (success == false) {
+				error = object.getString("error");
+				Log.e("getResponsesList - error: ", error);
+			} else {
+				JSONArray gs = object.getJSONArray("items");
+				int count = gs.length();
+				if (count > 0) {
+					for (int i = 0; i < count; i++) {
+						JSONObject g = gs.getJSONObject(i);
+						list.add(g);
+					}
+				}
+			}
+		} catch (Exception ex) {
+			Log.e("JSON err", ex.getMessage());
+		}
+		adapter = new ResponsesAdapter(ResponsesActivity.this, list);
 		responses_thread_list.setAdapter(adapter);
-		
+
 	}
 
 }
