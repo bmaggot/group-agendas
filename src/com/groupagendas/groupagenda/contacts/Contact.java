ba30f17a9e11b9ce85aefe5d93959a65cb0fb514
Audrius6 <Audrius6@gmail.com>
2012-12-06 10:15:56 +0000

    added all values for contacts

--- src/com/groupagendas/groupagenda/contacts/Contact.java
+++ src/com/groupagendas/groupagenda/contacts/Contact.java
@@ -10,8 +10,10 @@ import com.groupagendas.groupagenda.utils.MapUtils;
 
 public class Contact extends Object implements Colored {
 	public int contact_id;
+	public String lid;
 	public String name;
 	public String lastname;
+	public String fullname;
 	public String email;
 	public String phone1;
 	public String phone1_code;
@@ -21,6 +23,7 @@ public class Contact extends Object implements Colored {
 	public String street;
 	public String zip;
 	public String visibility;
+	public String visibility2;
 	
 	public boolean image;
 	public String image_url;
@@ -30,7 +33,16 @@ public class Contact extends Object implements Colored {
 	
 	public long created;
 	public long modified;
+	public int reg_user_id;
 	public String agenda_view;
+	public String agenda_view2;
+	public String can_add_note;
+	public String time_start;
+	public String time_end;
+	public String all_day;
+	public String display_time_end;
+	public String type;
+	public String title;
 	public String registered;
 	public Map<String, String> groups = null;
 
@@ -65,8 +77,10 @@ public class Contact extends Object implements Colored {
 	public Contact(Context context, Cursor cur) {
 		this.internal_id = cur.getInt(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData._ID));
 		this.contact_id = cur.getInt(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.C_ID));
+		this.lid = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.LID));
 		this.name = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.NAME));
 		this.lastname = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.LASTNAME));
+		this.fullname = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.FULLNAME));
 
 		this.email = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.EMAIL));
 		this.phone1 = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.PHONE));
@@ -80,6 +94,7 @@ public class Contact extends Object implements Colored {
 		this.zip = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.ZIP));
 
 		this.visibility = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.VISIBILITY));
+		this.visibility2 = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.VISIBILITY2));
 
 		if (cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.IMAGE)).equals("1")) {
 			this.image = true;
@@ -96,7 +111,16 @@ public class Contact extends Object implements Colored {
 			this.remove_image = false;
 		}
 
+		this.reg_user_id = cur.getInt(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.REG_USER_ID));
 		this.agenda_view = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.AGENDA_VIEW));
+		this.agenda_view2 = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.AGENDA_VIEW2));
+		this.can_add_note = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.CAN_ADD_NOTE));
+		this.time_start = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.TIME_START));
+		this.time_end = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.TIME_END));
+		this.all_day = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.ALL_DAY));
+		this.display_time_end = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.DISPLAY_TIME_END));
+		this.type = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.TYPE));
+		this.title = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.TITLE));
 		this.registered = cur.getString(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.REGISTERED));
 
 		this.created = cur.getLong(cur.getColumnIndex(ContactsProvider.CMetaData.ContactsMetaData.CREATED));
