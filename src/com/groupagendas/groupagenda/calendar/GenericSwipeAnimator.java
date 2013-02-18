c65104b8e94f0ef584bc477fb95dd2e64f76c203
Tadas.Samerdokas <Tadas.Samerdokas@f1f71889-e9f9-ec9e-1ee5-2b23fdef212a>
2013-02-18 15:32:12 +0000

    Temporary animation removal, as requested

--- src/com/groupagendas/groupagenda/calendar/GenericSwipeAnimator.java
+++ src/com/groupagendas/groupagenda/calendar/GenericSwipeAnimator.java
@@ -1,12 +1,6 @@
 package com.groupagendas.groupagenda.calendar;
 
-import android.content.Context;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.Animation.AnimationListener;
-import android.view.animation.AnimationUtils;
-
-import com.groupagendas.groupagenda.R;
 
 /**
  * Allows to easily use default LTR and RTL swipe animations.<BR>
@@ -17,6 +11,10 @@ import com.groupagendas.groupagenda.R;
  */
 public class GenericSwipeAnimator {
 	public static void startAnimation(final View view, final boolean rtl, final Runnable viewUpdater) {
+		// anims temporarily removed, so
+		viewUpdater.run();
+		
+		/*
 		final Context ctx = view.getContext();
 		Animation anim = AnimationUtils.loadAnimation(ctx,
 				rtl ? R.anim.swipe_rtl_leave : R.anim.swipe_ltr_leave);
@@ -41,5 +39,6 @@ public class GenericSwipeAnimator {
 			}
 		});
 		view.startAnimation(anim);
+		*/
 	}
 }
