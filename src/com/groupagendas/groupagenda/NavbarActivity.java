083dd10de61484f2552a98ed2dce10f590e12462
meska.lt <meska.lt@gmail.com>
2013-02-26 14:37:17 +0000

    Enabled loading of remotely saved templates in DownloadAllDataTask

--- src/com/groupagendas/groupagenda/NavbarActivity.java
+++ src/com/groupagendas/groupagenda/NavbarActivity.java
@@ -876,7 +876,7 @@ public class NavbarActivity extends FragmentActivity {
 				//$FALL-THROUGH$
 				case 4: // Load event templates
 					if (DataManagement.networkAvailable) {
-//						DataManagement.getTemplatesFromRemoteDb(NavbarActivity.this);
+						DataManagement.getTemplatesFromRemoteDb(NavbarActivity.this);
 					}
 					
 					if (doUneedSleep) {
@@ -938,7 +938,7 @@ public class NavbarActivity extends FragmentActivity {
 				progressDialog.setMessage(getString(R.string.loading_groups));
 				break;
 			case 50:
-//				progressDialog.setMessage(getString(R.string.loading_templates));
+				progressDialog.setMessage(getString(R.string.loading_templates));
 				break;
 			case 60:
 				progressDialog.setMessage(getString(R.string.loading_events));
